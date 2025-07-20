74a3dd43744ffaa82cd895ca286e98df278ea090
Fnafke <elinbachvarov@gmail.com>
2025-07-20 19:51:53 +0200

    added a options menu for resolutions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import ctypes
 from tkinter import filedialog
+from pytube import YouTube
 
 root = Tk()
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
@@ -22,6 +23,10 @@ center_window(800, 650)
 # declare url variable
 url_var = StringVar()
 download_path_var = StringVar()
+resolution_var = StringVar()
+resolution_var.set("Select a resolution:")
+resolution_list = ["144p", "244p", "360p", "480p",
+                   "720p", "1080p"]
 status_message_var = StringVar()
 
 # clear the status message variable
@@ -46,12 +51,23 @@ def download():
 
     url = url_var.get()
     download_path = download_path_var.get()
+    resolution = resolution_var.get()
+
+    if len(url.strip()) < 1:
+        status_message_var.set("URL is required!")
+        return
 
     if len(download_path.strip()) < 1:
         status_message_var.set("Download path is required!")
+        return
+
+    if resolution not in resolution_list:
+        status_message_var.set("Select a valid resoltion!")
+        return
 
     try:
-        ...
+        yt = YouTube(url)
+        stream = yt.streams.get_by_resolution()
     except:
         ...
 
@@ -62,14 +78,19 @@ url_label = Label(
     root, text="Provide the url of the Youtube Video you would like to download!", font=('calibre', 10, 'bold'))
 url_entry = Entry(root, textvariable=url_var, font=(
     'calibre', 10, 'normal'), width=50)
+
 download_path_label = Label(
     root, text="Select your download path!", font=('calibre', 10, 'bold'))
 download_path_entry = Entry(root, textvariable=download_path_var, font=(
     'calibre', 10, 'normal'), width=50)
 download_path_select_button = Button(
     root, text="Browse", command=select_download_path)
+
+resolution_list_menu = OptionMenu(root, resolution_var, *resolution_list)
+
 url_submit_button = Button(root, text="Download", font=(
     'calibre', 10, 'normal'), command=download)
+
 status_message = Label(root, textvariable=status_message_var)
 
 # positioning of all elements
@@ -78,6 +99,7 @@ url_entry.grid(row=1, column=0)
 download_path_label.grid(row=2, column=0)
 download_path_entry.grid(row=3, column=0)
 download_path_select_button.grid(row=4, column=0)
+resolution_list_menu.grid(row=5, column=0)
 url_submit_button.grid(row=6, column=0)
 status_message.grid(row=7, column=0)
 root.grid_columnconfigure(0, weight=1)
